60dc796fc0ca734c2bdfc14d29de51fda6c93339
Neyaprakash <141732433+Neyaprakah@users.noreply.github.com>
2024-07-23 17:20:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let arr=[];
+function accept(){
+    let str=document.getElementById("input").value;
+    arr.push(str);
+    console.log(arr);
+}
+function decline(){
+    window.alert("Ask me a Question");
+}
